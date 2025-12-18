219c2995e185e437372a0d13162f2b96412dafee
Guilherme <guibferrarezi@gmail.com>
2025-12-18 18:15:16 -0300

    perfil js alterado

--- frontend/js/perfil.js
+++ frontend/js/perfil.js
@@ -1,26 +1,9 @@
-fetch("/me")
-    .then(res => {
-        if (res.status === 401) {
-            window.location.href = "./frontend/login.html";
-            return;
-        }
-        return res.json();
-    })
-    .then(user => {
-        if (!user) return;
+document.addEventListener("DOMContentLoaded", async () => {
+	const res = await fetch("/me");
+	const data = await res.json();
 
-        document.getElementById("nome").textContent =
-            user.nome || user.usuario;
-
-        document.getElementById("email").textContent = user.email;
-
-        if (user.foto) {
-            document.getElementById("foto").src = user.foto;
-        } else {
-            document.getElementById("foto").style.display = "none";
-        }
-    });
-
-document.getElementById("btn-logout").addEventListener("click", () => {
-    window.location.href = "/logout";
+	if (data.logged) {
+		document.getElementById("ola-usuario").textContent =
+			`Olá, ${data.nome}`;
+	}
 });
